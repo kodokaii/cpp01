11a59cc7f69004331e8be7413713126f75b019a6
Nils Laeremans <nlaerema@c1r6p5.42lehavre.fr>
2024-02-24 16:33:05 +0100

    use switch instruction

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: nlaerema <nlaerema@student.42lehavre.fr>	+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 10:58:17 by nlaerema          #+#    #+#             */
-/*   Updated: 2024/02/24 15:43:00 by nlaerema         ###   ########.fr       */
+/*   Updated: 2024/02/24 16:25:44 by nlaerema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,40 @@ void	Harl::complainFilter(const std::string &filter)
 {
 	unsigned int	index(Harl::levelToIndex(filter));
 
+	switch (index)
+	{
+		case 0: 
+			std::cout << "[ " << Harl::levelString[0] << " ]" << std::endl;
+			(this->*Harl::complainFunc[0])();
+			std::cout << std::endl;
+			__attribute__ ((fallthrough));
+		case 1:
+			std::cout << "[ " << Harl::levelString[1] << " ]" << std::endl;
+			(this->*Harl::complainFunc[1])();
+			std::cout << std::endl;
+			__attribute__ ((fallthrough));
+		case 2:
+			std::cout << "[ " << Harl::levelString[2] << " ]" << std::endl;
+			(this->*Harl::complainFunc[2])();
+			std::cout << std::endl;
+			__attribute__ ((fallthrough));
+		case 3:
+			std::cout << "[ " << Harl::levelString[3] << " ]" << std::endl;
+			(this->*Harl::complainFunc[3])();
+			std::cout << std::endl;
+			break ;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
+}
+
+//much better than a switch
+
+/* 
+void	Harl::complainFilter(const std::string &filter)
+{
+	unsigned int	index(Harl::levelToIndex(filter));
+
 	if (index == LEVEL_COUNT)
 		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 	else
@@ -73,3 +107,4 @@ void	Harl::complainFilter(const std::string &filter)
 		}
 	}
 }
+*/
